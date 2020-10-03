966b5541d2f4a3c3576c2425df9519f30fd04c7e
chanul94 <cksdnfwkd@naver.com>
2020-10-03 23:53:42 +0900

    Implement webpack dev mode

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,18 @@
 import _ from 'lodash';
+import printMe from './print.js';
+
 
 function component() {
   const element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  const btn = document.createElement('button');
+
+  element.innerHTML = _.join(['안녕하세요', '웹팩입니다.'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
   return element;
 }
 
