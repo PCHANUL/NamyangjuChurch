4a1d01984245c8ae41ed504e0802564f7a074dd5
chanul94 <cksdnfwkd@naver.com>
2020-10-21 14:19:13 +0900

    Fix getData method

--- client/src/pages/VideoList.js
+++ client/src/pages/VideoList.js
@@ -1,60 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 import { gql, useQuery } from '@apollo/client';
 
-const GET_USERS = gql`
-  {
-    people {
-      id
-      name
-      age
-      gender
-    }
-  }
-`
-
-const videoData = [
-  {
-    title: '메시지 제목',
-    date: '201006',
-    desc: '여기에 메시지의 요약내용이 들어갑니다. 지금 생각하기로는 두줄이나 세줄이 적당한 것 같습니다.',
-    url: 'youtube/url'
-  },
-  {
-    title: '메시지 제목',
-    date: '201006',
-    desc: '여기에 메시지의 요약내용이 들어갑니다. 지금 생각하기로는 두줄이나 세줄이 적당한 것 같습니다.',
-    url: 'youtube/url'
-  },
-  {
-    title: '메시지 제목',
-    date: '201006',
-    desc: '여기에 메시지의 요약내용이 들어갑니다. 지금 생각하기로는 두줄이나 세줄이 적당한 것 같습니다.',
-    url: 'youtube/url'
-  },
-  {
-    title: '메시지 제목',
-    date: '201006',
-    desc: `여기에 메시지의 요약내용이 들어갑니다. 지금 생각하기로는 두줄이나 세줄이 적당한 것 같습니다.`,
-    url: 'youtube/url'
-  },
-  {
-    title: '메시지 제목',
-    date: '201006',
-    desc: `여기에 메시지의 요약내용이 들어갑니다. 지금 생각하기로는 두줄이나 세줄이 적당한 것 같습니다.`,
-    url: 'youtube/url'
-  },
-  {
-    title: '메시지 제목',
-    date: '201006',
-    desc: `여기에 메시지의 요약내용이 들어갑니다. 지금 생각하기로는 두줄이나 세줄이 적당한 것 같습니다.`,
-    url: 'youtube/url'
-  },
-] 
-
-const getData = async() => {
+const getData = async(callback) => {
   const data = await axios({
     url: 'http://localhost:4000/graphql',
     method: 'POST',
@@ -75,19 +25,30 @@ const getData = async() => {
       `
     }
   })
-
-  return data.data.data;
+  callback(data);
 }
 
-const VideoList = async(props) => {
-  // const { loading, error, data } = useQuery(GET_USERS);
-  // console.log(loading, error, data);
+const VideoList = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState({
+    people: []
+  })
+  
+  useEffect(() => {
+    getData((data) => {
+      setData(data.data.data);
+      setLoading(true);
+    });
+  }, [])
 
-  const data = await getData();
+  
+  
+  console.log('data: ', data);
 
   return (
     <div id='videoList'>
       {
+        loading &&
         data.people.map((data, i) => {
           return (
           <div class='video' key={data.id}>
