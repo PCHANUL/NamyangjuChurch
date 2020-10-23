4243714704014c6788eaa35857d3e1b9832c9deb
chanul94 <cksdnfwkd@naver.com>
2020-10-23 17:23:15 +0900

    Update Admin dataBox

--- client/src/pages/admin.js
+++ client/src/pages/admin.js
@@ -19,8 +19,7 @@ function Admin(props) {
     window.scroll(0,0);
     getData(async (getData) => {
       await setData(getData);
-      setLoading(true);
-      console.log('asdf')
+      setTimeout(setLoading(true), 2000);
     })
   }, [tab, subTab])
 
@@ -31,36 +30,68 @@ function Admin(props) {
       <div id='addBox'>
         <h2 style={{fontSize: '2vw'}}>관리자 페이지</h2> 
       </div>
+
       <Tab 
         tab={tab}
         setTab={setTab}
         subTab={subTab}
         setSubTab={setSubTab}
       />
+      
       <button id='addBtn' onClick={() => props.history.push('/admin/edit')}>
         <img id='addFileIcon' src={addIcon} />
       </button> 
-      <>
-        {
-          loading &&
-          data[tab].details[subTab].posts.map((data) => {
-            let createdDate = new Date(JSON.parse(data.createdAt))
-            console.log(data)
+     
+      <DataBox
+        data={data}
+        loading={loading}
+        tab={tab}
+        subTab={subTab}
+      />
+    </div>
+  )
+}
+
+function transDate(date) {
+  let rawDate = new Date(JSON.parse(date));
+  let year = rawDate.getFullYear();
+  let month = rawDate.getMonth() + 1;
+  let day = rawDate.getDate();
+  
+  return `${year}. ${month}. ${day}`
+}
+
+function DataBox({ loading, data, tab, subTab }) {
+  return (
+    <>
+      {
+        loading ? (
+          data[tab].details[subTab].posts.map((data, index) => {
+            let createdDate = transDate(data.createdAt)
             return (
-              <div id={data.id} className='dataBox'>
-                <h3 className='dataTitle' >{`${data.title} / ${createdDate}`}</h3>
-                <button className='dataButton' onClick={() => window.confirm('삭제하시겠습니까?')}>
-                  <img className='deleteIcon' src={deleteIcon}></img>
-                </button>
-                <button className='dataButton' onClick={() => props.history.push('/admin/edit')} >
-                  <img className='updateIcon' src={updateIcon}></img>
-                </button>
+              <div key={`data_${index}`} className='dataBox'>
+                <div className='textBox'>
+                  <h3 className='title'>{ data.title }</h3>
+                  <p className='date'>{ createdDate }</p>
+                </div>
+                <div className='buttonBox'>
+                  <button className='dataButton' onClick={() => window.confirm('삭제하시겠습니까?')}>
+                    <img className='deleteIcon' src={deleteIcon}></img>
+                  </button>
+                  <button className='dataButton' onClick={() => props.history.push('/admin/edit')} >
+                    <img className='updateIcon' src={updateIcon}></img>
+                  </button>
+                </div>
               </div>
             )
           })
-        }
-      </>
-    </div>
+        ) : (
+          <div className='dataBox'>
+            <h3 className='dataTitle' >Loading...</h3>
+          </div>
+        )
+      }
+    </>
   )
 }
 
@@ -71,8 +102,8 @@ function Tab({ tab, setTab, subTab, setSubTab }) {
         {
           ['말씀', '소식'].map((ele, idx) => {
             return idx === tab
-          ? <div class='adminTab selected' onClick={() => setTab(idx)}>{ele}</div>
-          : <div class='adminTab' onClick={() => setTab(idx)}>{ele}</div>
+          ? <div key={`tab_${idx}`} class='adminTab selected' onClick={() => setTab(idx)}>{ele}</div>
+          : <div key={`tab_${idx}`} class='adminTab' onClick={() => setTab(idx)}>{ele}</div>
           })
         }
       </div>
@@ -81,12 +112,14 @@ function Tab({ tab, setTab, subTab, setSubTab }) {
           {
             ['주일', '수요', '금요', '새벽', '기도수첩'].map((ele, idx) => {
               return idx === subTab 
-              ? <div class='subTab selected' onClick={() => setSubTab(idx)}>{ele}</div>
-              : <div class='subTab' onClick={() => setSubTab(idx)}>{ele}</div>
+              ? <div key={`subTab_${idx}`} class='subTab selected' onClick={() => setSubTab(idx)}>{ele}</div>
+              : <div key={`subTab_${idx}`} class='subTab' onClick={() => setSubTab(idx)}>{ele}</div>
             })
           }
         </div>
       }
+      <div className='extraDiv left'/>
+      <div className='extraDiv right'/>
     </div>
   )
 }
