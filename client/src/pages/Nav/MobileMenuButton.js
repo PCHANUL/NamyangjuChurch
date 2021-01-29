70044042459ed6ec592c17395ee6f0c05d9458ea
chanul94 <cksdnfwkd@naver.com>
2021-01-30 01:13:48 +0900

    Update drawerMenu

--- client/src/pages/Nav/MobileMenuButton.js
+++ client/src/pages/Nav/MobileMenuButton.js
@@ -13,19 +13,19 @@ export default function MobileMenuButton() {
       <div id='drawerBackground' className='' onTouchStart={() => clickFunction()}></div>
       <div id='drawerMenu' className='drawerMenuOpen'>
         <a onClick={() => window.location = "/"} className='menuBtn'>
-          홈
+          <h1>홈</h1>
         </a>
         <Link to="/contentlist" className='menuBtn' onClick={() => {
           appStore.setVideoList(0, 0);
           clickFunction();
         }}>
-          말씀 보기
+          <h1>말씀<br/>보기</h1>
         </Link>
         <Link to="/contentlist" className='menuBtn' onClick={() => {
           appStore.setVideoList(1, 0);
           clickFunction();
         }}>
-          교회 소식
+          <h1>교회<br/>소식</h1>
         </Link>
         <div></div>
       </div>
