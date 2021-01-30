e73f3756de824ed87959892440708520029d5d84
chanul94 <cksdnfwkd@naver.com>
2021-01-30 23:38:18 +0900

    Implement skeleton loading ui

--- client/src/pages/ContentList/ContentList.js
+++ client/src/pages/ContentList/ContentList.js
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 import { useAppStore } from '../../state/appContext';
 import { useObserver } from 'mobx-react';
 
+import './contentListLoading.css';
+
 // component
 import VideoContent from './VideoContent';
 import PhotoContent from './PhotoContent';
@@ -10,10 +12,14 @@ import PhotoContent from './PhotoContent';
 export default function ContentList() {
   const appStore = useAppStore();
   const [dataList, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useObserver(() => {
     useEffect(() => {
-      appStore.getContentList((result) => setData(result));
+      appStore.getContentList((result) => {
+        setData(result);
+        setTimeout(() => setLoading(false), 1000);
+      });
     }, [
       appStore.page, 
       appStore.verse, 
@@ -25,6 +31,48 @@ export default function ContentList() {
     ])
   });
 
+  if (loading) {  // loading ui
+    if (appStore.selectedCategory === 0) {  // videoList 
+      return (
+        <>
+          {
+            [0,0,0,0,0,0,0,0,0,0].map(() => {
+              return (
+                <div className='videoDiv-loading'>
+                  <div className='videoInfo-loading'>
+                    <div className='videoTitle-loading'></div>
+                    <div className='videoDate-loading'></div>
+                  </div>
+                  <div className='bibleVerse-loading'></div>
+                </div>
+              )}
+            )
+          }
+        </>
+      )
+    } else {  // photoList
+      return (
+        <div id='pictureListDiv'>
+          {
+            [0,0,0,0,0,0,0,0,0].map(() => {
+              return (
+                <div className='pictureDiv'>
+                  <div className='photoDiv-loading'></div>
+                  <div className='photoInfo-loading'>
+                    <div className='photoTitle-loading'></div>
+                    <div className='photoDate-loading'></div>
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+      )
+
+    }
+  }
+
+
   // 데이터가 없는 경우
   if (dataList.length === 0) {
     return (
@@ -35,13 +83,15 @@ export default function ContentList() {
     )
   }
 
+  // 데이터가 있는 경우
   if (appStore.selectedCategory === 0) {
-    return dataList.map((data, i) => <VideoContent data={data} key={i} />)
+    return dataList.map((data, i) => <VideoContent data={data} key={i} />);
   } else {
     return (
       <div id='pictureListDiv'>
         { dataList.map((data, i) => <PhotoContent data={data} key={i} />) }
       </div>
-    )
+    );
   }
+
 }
